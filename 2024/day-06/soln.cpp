b8e3ac3c0e8f1cbe2be17e63ac1d397dae70095c
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-12-13 01:53:12 +0530

    day 6 & 7

---
+++ 2024/day-06/soln.cpp
@@ -0,0 +1,162 @@
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+void printmap(const std::vector<std::string> &map) {
+    for (const auto &s : map) std::cout << s << std::endl;
+}
+
+std::vector<std::string> read_file(std::string file_name) {
+    std::ifstream input_file(file_name);
+    if (!input_file.is_open()) {
+        std::cerr << "Failed to open " << file_name << std::endl;
+        exit(1);
+    }
+
+    std::string line;
+    std::vector<std::string> arr;
+    while (std::getline(input_file, line)) {
+        arr.push_back(line);
+    }
+
+    return arr;
+}
+
+bool inbound(std::vector<std::string> &map, int i, int j) {
+    int rows = map.size();
+    int cols = map[0].length();
+    return (i >= 0 && i < rows && j >= 0 && j < cols);
+}
+
+void move(std::vector<std::string> &map, int &i, int &j, int &dir) {
+    int n_i = i, n_j = j;
+
+    switch (dir) {
+        case 0: // Up
+            --n_i;
+            break;
+        case 1: // Left
+            ++n_j;
+            break;
+        case 2: // Down
+            ++n_i;
+            break;
+        case 3: // Right
+            --n_j;
+            break;
+    }
+
+    if (inbound(map, n_i, n_j) && map[n_i][n_j] == '#') {
+        dir = (dir + 1) % 4;
+    } else {
+        i = n_i;
+        j = n_j;
+    }
+}
+
+void part1(std::vector<std::string> map) {
+    int sol = 0;
+    int rows = map.size();
+    int cols = map[0].length();
+    int i, j;
+    bool brfl = false;
+    for (i = 0; i < rows; ++i) {
+        for (j = 0; j < cols; ++j) {
+            if (map[i][j] == '^') {
+                brfl = true;
+                break;
+            }
+        }
+        if (brfl) break;
+    }
+
+    int dir = 0;
+    while (i >= 0 && i < rows && j >= 0 && j < cols) {
+        sol += (map[i][j] != 'X');
+        map[i][j] = 'X';
+        move(map, i, j, dir);
+    }
+
+    std::cout << sol << std::endl;
+}
+
+bool put_obstacle(std::vector<std::string> &map, int i, int j, int dir, const int &idx, const int &jdx, std::set<std::tuple<int, int>> &obstacles) {
+    switch (dir) {
+        case 0: // Up
+            --i;
+            break;
+        case 1: // Left
+            ++j;
+            break;
+        case 2: // Down
+            ++i;
+            break;
+        case 3: // Right
+            --j;
+            break;
+    }
+    if (!inbound(map, i, j) || (i == idx && j == jdx) || map[i][j] == '#' || obstacles.count(std::make_tuple(i, j))) {
+        return false;
+    }
+    map[i][j] = '#';
+    obstacles.insert(std::make_tuple(i, j));
+    return true;
+}
+
+int check_loop(std::vector<std::string> map, int i, int j, int dir, const int &idx, const int &jdx, const std::set<std::tuple<int, int, int>> &map_visits, std::set<std::tuple<int, int>> &obstacles) {
+    if (!put_obstacle(map, i, j, dir, idx, jdx, obstacles)) return 0;
+
+    std::set<std::tuple<int, int, int>> visited = map_visits;
+    dir = (dir + 1) % 4;
+
+    int count = 0;
+    while (inbound(map, i, j)) {
+        auto state = std::make_tuple(i, j, dir);
+        if (visited.count(state)) {
+            return 1;
+        }
+        visited.insert(state);
+        move(map, i, j, dir);
+    }
+    return 0;
+}
+
+void part2(std::vector<std::string> map) {
+    int sol = 0;
+    int rows = map.size();
+    int cols = map[0].length();
+    int i, j;
+    bool brfl = false;
+    for (i = 0; i < rows; ++i) {
+        for (j = 0; j < cols; ++j) {
+            if (map[i][j] == '^') {
+                brfl = true;
+                break;
+            }
+        }
+        if (brfl) break;
+    }
+
+    const int idx = i, jdx = j;
+    int dir = 0;
+    std::set<std::tuple<int, int, int>> visited;
+    std::set<std::tuple<int, int>> obstacles;
+    while (inbound(map, i, j)) {
+        visited.insert(std::make_tuple(i, j, dir));
+        sol += check_loop(map, i, j, dir, idx, jdx, visited, obstacles);
+        move(map, i, j, dir);
+    }
+
+    std::cout << sol << std::endl;
+}
+
+int main() {
+    auto arr = read_file("input.txt");
+    part1(arr);
+    part2(arr);
+
+    return 0;
+}
---
+++ 2024/day-07/soln.cpp
@@ -0,0 +1,90 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::vector<long long>> read_file(std::string file_name) {
+    std::ifstream input_file(file_name);
+    if (!input_file.is_open()) {
+        std::cerr << "Failed to open " << file_name << std::endl;
+        exit(1);
+    }
+
+    std::string line;
+    std::vector<std::vector<long long>> arr;
+    while (std::getline(input_file, line)) {
+        std::vector<long long> nums;
+        long long i = 0;
+        long long delim = line.find(": ");
+        nums.push_back(std::stoll(line.substr(i, delim)));
+        i = delim + 2;
+        delim = line.find(' ', i);
+        while (delim != std::string::npos) {
+            nums.push_back(std::stoll(line.substr(i, delim)));
+            i = delim + 1;
+            delim = line.find(' ', i);
+        }
+        nums.push_back(std::stoll(line.substr(i)));
+        arr.push_back(nums);
+    }
+
+    return arr;
+}
+
+bool is_valid(std::vector<long long> &nums, long long op = 0, long long i = 1, long long sum = 0) {
+    long long res = nums[0];
+    long long len = nums.size();
+
+    if (op == 0) {
+        sum += nums[i];
+    } else {
+        sum *= nums[i];
+    }
+
+    if (i == len - 1) {
+        return res == sum;
+    }
+    return is_valid(nums, 0, i + 1, sum) || is_valid(nums, 1, i + 1, sum);
+}
+
+void part1(std::vector<std::vector<long long>> arr) {
+    long long sol = 0;
+    for (auto nums : arr) {
+        sol += is_valid(nums) ? nums[0] : 0;
+    }
+    std::cout << sol << std::endl;
+}
+
+bool is_valid_2(std::vector<long long> &nums, long long op = 0, long long i = 1, long long sum = 0) {
+    long long res = nums[0];
+    long long len = nums.size();
+
+    if (op == 0) {
+        sum += nums[i];
+    } else if (op == 1) {
+        sum *= nums[i];
+    } else {
+        sum = std::stoll(std::to_string(sum) + std::to_string(nums[i]));
+    }
+
+    if (i == len - 1) {
+        return res == sum;
+    }
+    return is_valid_2(nums, 0, i + 1, sum) || is_valid_2(nums, 1, i + 1, sum) || is_valid_2(nums, 2, i + 1, sum);
+}
+
+void part2(std::vector<std::vector<long long>> arr) {
+    long long sol = 0;
+    for (auto nums : arr) {
+        sol += is_valid_2(nums) ? nums[0] : 0;
+    }
+    std::cout << sol << std::endl;
+}
+
+int main() {
+    auto arr = read_file("input.txt");
+    part1(arr);
+    part2(arr);
+
+    return 0;
+}
