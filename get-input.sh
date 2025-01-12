b112d62fb1f474cc1b99724bf21b5a42703fb07b
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2025-01-12 16:27:33 +0530

    updated get-input script
    Added more sanity checks and refactor the code

--- get-input.sh
+++ get-input.sh
@@ -1,18 +1,50 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]]; then
-    echo "Requires 2 arguments:"
-    echo "    1. year (e.g. 2024)"
-    echo "    2. day (e.g. 5)"
-    exit 1
-fi
+function validate-input() {
+    if [[ $# -ne 2 ]]; then
+        echo "Requires 2 arguments:"
+        echo "    1. year (e.g. 2024)"
+        echo "    2. day (e.g. 5)"
+        exit 1
+    fi
 
-source .env
+    local curr_year
+    curr_year=$(date +%Y)
 
-input=$(curl "https://adventofcode.com/$1/day/$2/input" \
-    -H "Cookie: session=$SESSION_ID")
+    if [[ $1 -lt 2015 || $1 -gt $curr_year ]]; then
+        echo "Year should be in between 2015 and $curr_year"
+        exit 1
+    fi
 
-dir=$(printf "%d/day-%02d" "$1" "$2")
-mkdir -p "$dir/cpp"
-echo "$input" >"$dir/input.txt"
-cp "template/template.cpp" "$dir/cpp/soln.cpp"
+    if [[ $2 -lt 1 || $2 -gt 25 ]]; then
+        echo "Day should be in between 1 and 25"
+        exit 1
+    fi
+
+    year=$1
+    day=$2
+}
+
+function get-input() {
+    local input
+    input=$(curl "https://adventofcode.com/$year/day/$day/input" \
+        -H "Cookie: session=$SESSION_ID")
+
+    local dir
+    dir=$(cd -- "$(dirname "$0")" && pwd)
+    dir=$(printf "%s/%d/day-%02d" "$dir" "$year" "$day")
+
+    mkdir -p "$dir/cpp"
+    echo "$input" >"$dir/input.txt"
+    cp "template/template.cpp" "$dir/cpp/soln.cpp"
+}
+
+function main() {
+    local day
+    local year
+    source .env
+    validate-input "$@"
+    get-input
+}
+
+main "$@"
